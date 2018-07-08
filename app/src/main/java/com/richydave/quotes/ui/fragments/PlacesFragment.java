f129db04d6cc0322369d8b5e1340a660ce9ad2e0
Richard Ekong <richard.ekong@anotap.com>
2018-07-08 15:52:06 +0300

    fixed bugs in PlacesFragment.java

--- app/src/main/java/com/richydave/quotes/ui/fragments/PlacesFragment.java
+++ app/src/main/java/com/richydave/quotes/ui/fragments/PlacesFragment.java
@@ -1,12 +1,11 @@
 package com.richydave.quotes.ui.fragments;
 
-import android.os.Bundle;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,19 +15,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.richydave.quotes.Constant;
 import com.richydave.quotes.R;
+import com.richydave.quotes.util.LocationUtil;
 import butterknife.ButterKnife;
-
 import static com.richydave.quotes.Constant.BEARING;
 import static com.richydave.quotes.Constant.BIRTH_PLACE;
-import static com.richydave.quotes.Constant.LOCATION;
+import static com.richydave.quotes.Constant.LOCAL_LOCATION;
+import static com.richydave.quotes.Constant.ONLINE_LOCATION;
 import static com.richydave.quotes.Constant.TILT_ANGLE;
 import static com.richydave.quotes.Constant.ZOOM_LEVEL;
 
 
-public class BirthPlaceFragment extends Fragment implements OnMapReadyCallback {
-
-
+public class PlacesFragment extends Fragment implements OnMapReadyCallback {
 
 
     @Override
@@ -63,14 +62,7 @@ public class BirthPlaceFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        String birthPlace = "";
-        LatLng coordinates = new LatLng(0, 0);
-        if (getArguments() != null) {
-            Bundle locationDetails = getArguments();
-            birthPlace = locationDetails.getString(BIRTH_PLACE);
-            coordinates = locationDetails.getParcelable(LOCATION);
-        }
-        googleMap.addMarker(new MarkerOptions().position(coordinates).title(birthPlace));
+        LatLng coordinates = configureGoogleMap(googleMap);
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         CameraPosition cameraPosition = CameraPosition.builder()
                 .target(coordinates)
@@ -82,4 +74,30 @@ public class BirthPlaceFragment extends Fragment implements OnMapReadyCallback {
 
     }
 
+    private LatLng configureGoogleMap(GoogleMap googleMap) {
+        String birthPlace;
+        LatLng coordinates = null;
+        String quotePlace;
+        try {
+            Bundle locationDetails = getArguments();
+            String quoteTag = locationDetails.getString(Constant.QUOTE_TAG);
+            if (quoteTag.equals(Constant.ONLINE_QUOTE)) {
+                birthPlace = locationDetails.getString(BIRTH_PLACE);
+                coordinates = locationDetails.getParcelable(ONLINE_LOCATION);
+                googleMap.addMarker(new MarkerOptions()
+                        .position(coordinates)
+                        .title(birthPlace));
+            }
+            if (quoteTag.equals(Constant.LOCAL_QUOTE)) {
+                coordinates = locationDetails.getParcelable(LOCAL_LOCATION);
+                quotePlace = LocationUtil.getPlaceFromLocation(getContext(), coordinates);
+                googleMap.addMarker(new MarkerOptions()
+                        .position(coordinates)
+                        .title(quotePlace));
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return coordinates;
+    }
 }
